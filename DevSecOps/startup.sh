24c02e64f609c5e5b95ac233473be9a74b492610
tapansirol <tapansirole@gmail.com>
2019-01-09 04:46:15 -0800

    calling each script individually

--- DevSecOps/startup.sh
+++ DevSecOps/startup.sh
@@ -3,7 +3,11 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 cd $SCRIPTPATH
 #cd /home/ubuntu/Tapan/DevSecOps
-sudo ./start-all-containers.sh
+sudo ./exec-sonar-docker-compose.sh
+
+sudo ./exec-udeploy-docker-compose.sh
+
+sudo ./exec-uvelocity-docker-compose.sh
 
 cd $(pwd)/Jenkins/Jenkins-automation
 sudo ./startup.sh
